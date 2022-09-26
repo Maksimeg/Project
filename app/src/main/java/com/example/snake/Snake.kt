418feda6501090d901e7c8f2e7d5a398407f1dff
Максим <90345313+Maksimeg@users.noreply.github.com>
2022-09-26 14:21:10 +0300

    Project

--- app/src/main/java/com/example/snake/Snake.kt
+++ app/src/main/java/com/example/snake/Snake.kt
@@ -4,7 +4,7 @@ const val  START_GAME_SPEED = 500L
 const val  MINIMUM_GAME_SPEED = 200L
 
 object Snake{
-    var nextMove: () -> Unit = {}// метод
+    var nextMove: () -> Unit = {}
     var play = true
     private val thread: Thread
     var gameSpeed = START_GAME_SPEED
